3451683f2645a82d4e3d45b01a96aab5463e12f4
Dave Tillman <dtillman@pivotal.io>
2017-04-21 06:57:35 -0600

    Change to .csproj

--- scripts/appveyor_build.cmd
+++ scripts/appveyor_build.cmd
@@ -1,20 +1,9 @@
 :: @ECHO OFF
 
-:: Patch project.json files
-cd %APPVEYOR_BUILD_FOLDER%\scripts
-call npm install
-call node patch-project-json.js ../src/Pivotal.Discovery.Client/project.json %APPVEYOR_BUILD_VERSION% %APPVEYOR_REPO_TAG_NAME%
-cd %APPVEYOR_BUILD_FOLDER%
-
-:: Restore packages
-cd src
-dotnet restore
-cd ..\test
-dotnet restore
-cd ..
-
 :: Build packages
 cd %APPVEYOR_BUILD_FOLDER%
 cd src\Pivotal.Discovery.Client
-dotnet pack --configuration Release
+dotnet restore --configfile ..\..\nuget.config
+IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release)
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (dotnet pack --configuration Release --version-suffix %STEELTOE_VERSION_SUFFIX%)
 cd %APPVEYOR_BUILD_FOLDER%
--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
-export STEELTOE_VERSION="1.0.1"
-export DOTNET_INSTALL_DIR="$PWD/.dotnetsdk"
-export PATH="$DOTNET_INSTALL_DIR:$PATH"
-:: Patch project.json files
-cd ./scripts
-npm install
-node patch-project-json.js ../src/Pivotal.Discovery.Client/project.json $STEELTOE_VERSION-$TRAVIS_BRANCH-$TRAVIS_BUILD_NUMBER $TRAVIS_TAG
-cd ..
-cd src
-dotnet restore
-cd ../test
-dotnet restore
-cd ..
+
 cd src/Pivotal.Discovery.Client
-dotnet build --framework netstandard1.3 --configuration Release
+dotnet restore --configfile ../../nuget.config
 cd ../..
